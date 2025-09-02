c7edcaf2a3e21302c478427ca99670f66debeb06
Levio-Z <67247011+Levio-z@users.noreply.github.com>
2025-09-02 16:11:42 +0800

    feat: add 05-Interrupts-cpu_exceptions

--- src/lib.rs
+++ src/lib.rs
@@ -3,6 +3,8 @@
 #![feature(custom_test_frameworks)]
 #![test_runner(crate::test_runner)]
 #![reexport_test_harness_main = "test_main"]
+#![feature(abi_x86_interrupt)]
+pub mod interrupts;
 pub mod serial;
 pub mod vga_buffer;
 use core::panic::PanicInfo;
@@ -43,6 +45,7 @@ pub fn test_panic_handler(info: &PanicInfo) -> ! {
 #[cfg(test)]
 #[unsafe(no_mangle)]
 pub extern "C" fn _start() -> ! {
+    init(); // new
     test_main();
     #[allow(clippy::empty_loop)]
     loop {}
@@ -68,4 +71,8 @@ pub fn exit_qemu(exit_code: QemuExitCode) {
         let mut port = Port::new(0xf4);
         port.write(exit_code as u32);
     }
+}
+
+pub fn init() {
+    interrupts::init_idt();
 }
